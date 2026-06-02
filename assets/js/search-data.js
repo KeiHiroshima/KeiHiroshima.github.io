d3b0bf69ac85beaaf55337fc83f9f55f7f65bdf6
KeiHiroshima <102266977+KeiHiroshima@users.noreply.github.com>
2026-06-02 05:33:23 +0000

    Deploying to gh-pages from @ KeiHiroshima/KeiHiroshima.github.io@b3342d399f69ca73d85adda27ac60f5da1c8d751 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -1,7 +1,7 @@
 
 const currentUrl = window.location.href;
-const siteUrl = "https://keihiroshima.github.io";
-let updatedUrl = currentUrl.replace("https://keihiroshima.github.io", "");
+const siteUrl = "https://KeiHiroshima.github.io";
+let updatedUrl = currentUrl.replace("https://KeiHiroshima.github.io", "");
 if (currentUrl.length == updatedUrl.length && currentUrl.startsWith("http://127.0.0.1")) {
   const otherSiteUrl = siteUrl.replace("localhost", "127.0.0.1");
   updatedUrl = currentUrl.replace(otherSiteUrl + "", "");
