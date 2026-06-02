dbcce3009a4f3acdff44646dd4980a8d6285d35d
KeiHiroshima <102266977+KeiHiroshima@users.noreply.github.com>
2026-06-02 07:56:23 +0000

    Deploying to gh-pages from @ KeiHiroshima/KeiHiroshima.github.io@eeca04c450a12ef5bc4e11ae152782cc34f80169 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -88,16 +88,19 @@ ninja.data = [{
           description: "",
           section: "",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
+            },},{id: "news-tba",
+          title: 'TBA',
           description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
+          section: "News",},{id: "news-um-anúncio-longo-com-detalhes",
+          title: 'Um anúncio longo com detalhes',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-um-anúncio-simples-em-uma-linha-com-markdown-emoji-sparkles-smile",
+          title: 'Um anúncio simples em uma linha com Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-tba",
+          title: 'TBA',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
